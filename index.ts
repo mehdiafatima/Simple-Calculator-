bbac9213ff29dd5f4c803060c28046afb0badcd4
Mehdia Fatima <mehdiaff9876@gmail.com>
2024-03-27 18:43:42 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer" 
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+      message: "select one of the operatos to perform operation",
+      type: "list",
+      name: "operators",
+      choices: ["Addition","Subraction", "Multiplication", "Division"]},
+  ]);
+  // conditional statement
+if (answer.operators === "Addition") {
+    console.log( answer.firstNumber + answer.secondNumber);
+} else if (answer.operators === "Subtraction") {
+    console.log( answer.firstNumber - answer.secondNumber)
+} else if (answer.operators === "Multiplication") {
+    console.log( answer.firstNumber * answer.secondNumber)
+} else if (answer.operators === "Division") {
+    console.log( answer.firstNumber / answer.secondNumber) 
+} else {
+ console.log("Please select valid operator")
+}
+
